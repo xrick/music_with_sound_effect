622a137c743b81a5f9f2303dd82d0f1c7c4c9fa9
xrick <xrickliao@gmail.com>
2024-09-17 03:48:21 +0800

    add

---
+++ src/demo.c
@@ -0,0 +1,18 @@
+/*
+libavutil      58. 29.100 / 58. 29.100
+  libavcodec     60. 31.102 / 60. 31.102
+  libavformat    60. 16.100 / 60. 16.100
+  libavdevice    60.  3.100 / 60.  3.100
+  libavfilter     9. 12.100 /  9. 12.100
+  libswscale      7.  5.100 /  7.  5.100
+  libswresample   4. 12.100 /  4. 12.100
+  libpostproc    57.  3.100 / 57.  3.100
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
